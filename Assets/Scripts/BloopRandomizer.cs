69074efd80ce238a98890e8409aa63e9ed4e647b
Xaenyn <36776050+Xaenyn@users.noreply.github.com>
2018-04-29 12:30:10 -0400

    Added some FX, made BloopRandomizer script

---
+++ Assets/Scripts/BloopRandomizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloopRandomizer : MonoBehaviour {
+	public AudioSource src;
+	public AudioClip clip1;
+	public AudioClip clip2;
+	public AudioClip clip3;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	public void PlayRandomBloop ()
+	{
+		int checker = Random.Range (1, 4);
+		if (checker == 1) 
+		{
+			src.clip = clip1;
+		}
+		if (checker == 2) 
+		{
+			src.clip = clip2;
+		}
+		if (checker == 3) 
+		{
+			src.clip = clip3;
+		}
+		src.Play();
+	}
+}
