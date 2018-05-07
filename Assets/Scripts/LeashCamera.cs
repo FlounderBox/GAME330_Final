cb63d3d06f231673afacfad61351fa596c4e5ff1
FlounderBox <m.thomas.2546@gmail.com>
2018-05-07 18:47:32 -0400

    Improved leash camera smoothing

--- Assets/Scripts/LeashCamera.cs
+++ Assets/Scripts/LeashCamera.cs
@@ -8,6 +8,7 @@ public class LeashCamera : MonoBehaviour
     public PostProcessingProfile postProcessing;
     public Transform Target;
     public float LeashLength = 5;
+    public float RotateSpeed = 3;
 
 
     private void Start()
@@ -24,14 +25,20 @@ public class LeashCamera : MonoBehaviour
             postProcessing.depthOfField.settings = _dof;
         }
 
-        Vector3 targetCamPos = transform.position;
+        //Vector3 targetCamPos = transform.position;
 
-        if (Vector3.Distance(Target.position, transform.position) > LeashLength)
-        {
-            targetCamPos = Target.position + Vector3.up * 0.5f;
-        }
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, 0.4f * Time.deltaTime);
-        transform.LookAt(Target.transform);
+        Vector3 offsetTargetPos = Target.position + Vector3.up * 0.5f;
+        Vector3 targetCamPos = offsetTargetPos + Vector3.Normalize(transform.position- offsetTargetPos) * LeashLength; 
+
+        //if (Vector3.Distance(Target.position, transform.position) > LeashLength)
+        //{
+        //    targetCamPos = Target.position + Vector3.up * 0.5f;
+        //}
+        //transform.position = Vector3.Lerp(transform.position, targetCamPos, 0.4f * Time.deltaTime);
+        transform.position += (targetCamPos - transform.position) * 0.1f * Time.timeScale;
+
+        //transform.rotation += Quaternion.Euler((Quaternion.LookRotation(Target.transform.position - transform.position, Vector3.up).eulerAngles - transform.rotation.eulerAngles * 0.1f * Time.timeScale));
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Target.transform.position - transform.position, Vector3.up), Time.deltaTime * RotateSpeed);
     }
 
     public void AddLength(float f)
